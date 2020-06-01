7bb40814dcf1e23ad5de05a8317b284bbe11fb2e
dukeyunz <duchg@mail2.sysu.edu.cn>
2020-06-01 16:50:14 +0800

    fix: use default serverless function handler

--- api/bot-webhook.go
+++ api/bot-webhook.go
@@ -1,51 +1,41 @@
 package handler
 
 import (
+	"encoding/json"
 	"fund/cryptoc"
 	"fund/data"
 	"fund/log"
-	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+	. "github.com/go-telegram-bot-api/telegram-bot-api"
+	"io/ioutil"
 	"net/http"
 	"os"
 )
 
-type ReqBody struct {
-	UpdateID int     `json:"update_id"`
-	Message  Message `json:"message"`
-}
+var bot *BotAPI
 
-type Message struct {
-	MessageID int    `json:"message_id"`
-	From      From   `json:"from"`
-	Chat      Chat   `json:"chat"`
-	Date      int    `json:"date"`
-	Text      string `json:"text"`
-}
-
-type From struct {
-	ID           int    `json:"id"`
-	IsBot        bool   `json:"is_bot"`
-	FirstName    string `json:"first_name"`
-	LanguageCode string `json:"language_code"`
-}
-
-type Chat struct {
-	ID        int    `json:"id"`
-	FirstName string `json:"first_name"`
-	Type      string `json:"type"`
-}
-
-func Handler(w http.ResponseWriter, req *http.Request) {
-	bot, err := tgbotapi.NewBotAPI(os.Getenv("BOT_TOKEN"))
+func init()  {
+	bot, err := NewBotAPI(os.Getenv("BOT_TOKEN"))
 	if err != nil {
 		log.Fatal("Init Bot error: ", err)
 	}
 
 	bot.Debug = true
-
 	log.Debug("Authorized on account %s", bot.Self.UserName)
+}
 
-	updates := bot.ListenForWebhook("/" + bot.Token)
+func handler(w http.ResponseWriter, r *http.Request) UpdatesChannel {
+	ch := make(chan Update, BotAPI{}.Buffer)
+	bytes, _ := ioutil.ReadAll(r.Body)
+	var update Update
+	json.Unmarshal(bytes, &update)
+	ch <- update
+
+	return ch
+}
+
+func Handler(w http.ResponseWriter, req *http.Request) {
+	updates := handler(w, req)
+
 	for update := range updates {
 		if update.Message == nil { // ignore any non-Message Updates
 			continue
@@ -61,9 +51,9 @@ func Handler(w http.ResponseWriter, req *http.Request) {
 			reply = "暂时无法理解： "+update.Message.Text
 		}
 
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, reply)
+		msg := NewMessage(update.Message.Chat.ID, reply)
 		msg.ReplyToMessageID = update.Message.MessageID
-		msg.ParseMode = tgbotapi.ModeHTML
+		msg.ParseMode = ModeHTML
 		//msg.ParseMode = tgbotapi.ModeMarkdown
 		bot.Send(msg)
 
