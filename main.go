c5be2dcfd08e21bdb807654df0fd6dbfacc13fa4
dukeyunz <dukeyunz@hotmail.com>
2021-11-16 13:29:24 +0800

    webhook

--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"fund/bot"
 	"fund/config"
+	"fund/cron"
 	"fund/db"
 	"fund/global"
 	"fund/log"
+	"fund/notifier"
+	"net/http"
+	"os"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 )
@@ -28,5 +33,38 @@ func init() {
 }
 
 func main() {
-	bot.Run()
+	// local
+	if len(os.Args) > 1 {
+		go cron.Update()
+		bot.Run()
+	} else {
+		port := config.EnvVariable("PORT")
+		fmt.Println("Server started on port:", port)
+		global.Bot.RemoveWebhook()
+		_, err = global.Bot.SetWebhook(tgbotapi.NewWebhook("https://thawing-scrubland-62700.herokuapp.com/" + global.Bot.Token))
+		if err != nil {
+			log.Fatal("%v", err)
+		}
+
+		updates := global.Bot.ListenForWebhook("/" + global.Bot.Token)
+		go http.ListenAndServe("0.0.0.0"+":"+port, SendEmail{})
+		go cron.Update()
+
+		for update := range updates {
+			bot.Handle(update)
+		}
+	}
+}
+
+type SendEmail struct{}
+
+func (SendEmail) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	log.Info("%v", r)
+	fmt.Println("定时发邮件任务")
+	e := &notifier.Email{
+		To:      "dukeyunz@hotmail.com",
+		Subject: "Fund notification",
+	}
+	e.Send("Test email from heroku every 5min.")
+	w.Write([]byte("ok"))
 }
