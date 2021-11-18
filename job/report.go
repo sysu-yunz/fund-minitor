b12d1d3803def276b14c70fa69926d8016f74d6f
dukeyunz <dukeyunz@hotmail.com>
2021-11-18 18:32:56 +0800

    add basic auth

--- job/report.go
+++ job/report.go
@@ -1,8 +1,11 @@
 package job
 
 import (
+	"fmt"
+	"fund/log"
 	"fund/notifier"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
@@ -10,10 +13,30 @@ import (
 // find schedulers in https://www.easycron.com/
 
 func DailyReport(c *gin.Context) {
+	username := os.Getenv("username")
+	password := os.Getenv("password")
+	u, p, ok := c.Request.BasicAuth()
+	if !ok {
+		fmt.Println("Error parsing basic auth")
+		c.String(http.StatusUnauthorized, "Error parsing basic auth")
+		return
+	}
+	if u != username {
+		log.Info("Username provided is correct: %s\n", u)
+		c.String(http.StatusUnauthorized, "Error parsing basic auth")
+		return
+	}
+	if p != password {
+		log.Info("Password provided is correct: %s\n", u)
+		c.String(http.StatusUnauthorized, "Error parsing basic auth")
+		return
+	}
+	log.Info("Username: %s\n", u)
+	log.Info("Password: %s\n", p)
 	e := &notifier.Email{
 		To:      "dukeyunz@hotmail.com",
 		Subject: "Fund notification",
 	}
-	e.Send("Test email from heroku every 5min.")
+	e.Send("Hello from heroku !")
 	c.String(http.StatusOK, "ok")
 }
